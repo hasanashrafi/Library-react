2405c64acb771814fc0b7565ba71e80e63caaf41
Hasan Ashrafi <yasashrafi@gmail.com>
2024-03-16 17:30:59 +0330

    create search box component

---
+++ src/components/SearchBox.jsx
@@ -0,0 +1,18 @@
+import { IoSearch } from "react-icons/io5";
+
+import styles from "./SearchBox.module.css"
+
+function SearchBox({ search, setSearch,searchHandler }) {
+    return (
+        <div className={styles.search}>
+            <input
+                type='text'
+                placeholder='search'
+                value={search}
+                onChange={(e) => setSearch(e.target.value.toLocaleLowerCase().trim())} />
+            <button onClick={searchHandler}><IoSearch /></button>
+        </div>
+    );
+}
+
+export default SearchBox;
