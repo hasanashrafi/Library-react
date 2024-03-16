4d384021c6adf212904a52fa27597aa64af69b8c
Hasan Ashrafi <yasashrafi@gmail.com>
2024-03-16 17:39:10 +0330

    create library app

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import Books from "./components/Books"
+import LayOut from "./layout/LayOut"
+
+function App() {
+
+
+  return (
+    <LayOut>
+      <Books/>
+    </LayOut>
+  )
+}
+
+export default App
